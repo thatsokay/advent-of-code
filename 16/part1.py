8ca63c9433bc73c809e5cf5923cee13e7915591d
Andre Wong <andre@andre-wong.com>
2019-02-11 21:58:15 +1000

    Day 16

---
+++ 16/part1.py
@@ -0,0 +1,40 @@
+ops = {
+    'addr': lambda reg, a, b, c: {**reg, c: reg[a] + reg[b]},
+    'addi': lambda reg, a, b, c: {**reg, c: reg[a] + b},
+    'mulr': lambda reg, a, b, c: {**reg, c: reg[a] * reg[b]},
+    'muli': lambda reg, a, b, c: {**reg, c: reg[a] * b},
+    'banr': lambda reg, a, b, c: {**reg, c: reg[a] & reg[b]},
+    'bani': lambda reg, a, b, c: {**reg, c: reg[a] & b},
+    'borr': lambda reg, a, b, c: {**reg, c: reg[a] | reg[b]},
+    'bori': lambda reg, a, b, c: {**reg, c: reg[a] | b},
+    'setr': lambda reg, a, b, c: {**reg, c: reg[a]},
+    'seti': lambda reg, a, b, c: {**reg, c: a},
+    'gtir': lambda reg, a, b, c: {**reg, c: int(a > reg[b])},
+    'gtri': lambda reg, a, b, c: {**reg, c: int(reg[a] > b)},
+    'gtrr': lambda reg, a, b, c: {**reg, c: int(reg[a] > reg[b])},
+    'eqir': lambda reg, a, b, c: {**reg, c: int(a == reg[b])},
+    'eqri': lambda reg, a, b, c: {**reg, c: int(reg[a] == b)},
+    'eqrr': lambda reg, a, b, c: {**reg, c: int(reg[a] == reg[b])},
+}
+
+def parse_sample(sample):
+    before, instruction, after = sample.strip().split('\n')
+    before = dict(enumerate(map(int, before[9:-1].split(', '))))
+    after = dict(enumerate(map(int, after[9:-1].split(', '))))
+    instruction = [int(x) for x in instruction.split()]
+    return instruction, before, after
+
+def possible_ops(instruction, before, after):
+    return {name for name, op in ops.items() if op(before, *instruction[1:]) == after}
+
+def count_threes(samples):
+    return sum((
+        True
+        for sample in samples
+        if len(possible_ops(*parse_sample(sample))) >= 3
+    ))
+
+if __name__ == '__main__':
+    with open('input.txt') as f:
+        samples = f.read().split('\n\n\n')[0].split('\n\n')
+    print(count_threes(samples))
---
+++ 16/part2.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+
+from part1 import ops, parse_sample, possible_ops
+
+def find_opcodes(samples):
+    opcodes = defaultdict(lambda: set(ops.keys()))
+    for instruction, before, after in map(parse_sample, samples):
+        opcodes[instruction[0]].intersection_update(possible_ops(instruction, before, after))
+        if len(opcodes[instruction[0]]) == 1:
+            for i in range(16):
+                if i != instruction[0]:
+                    opcodes[i] -= opcodes[instruction[0]]
+    return {opcode: ops[names.pop()] for opcode, names in opcodes.items()}
+
+def run_program(opcodes, instructions):
+    register = dict(enumerate((0, 0, 0, 0)))
+    for opcode, a, b, c in ([int(x) for x in instr.split()] for instr in instructions):
+        register = opcodes[opcode](register, a, b, c)
+    return register
+
+if __name__ == '__main__':
+    with open('input.txt') as f:
+        samples, program = f.read().split('\n\n\n')
+    opcodes = find_opcodes(samples.strip().split('\n\n'))
+    print(run_program(opcodes, program.strip().split('\n')))
