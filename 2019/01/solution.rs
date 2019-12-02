3dbf0c5a83e0916bed570ff18fb11b56cbe1388d
Andre Wong <andre@andre-wong.com>
2019-12-02 13:27:29 +1000

    Rust changes and formatting

--- 2019/01/solution.rs
+++ 2019/01/solution.rs
@@ -1,8 +1,8 @@
 use std::fs;
 
 fn fuel(mass: &i32) -> i32 {
-    let mut fuel_mass = mass / 3 - 2;
-    let mut total_fuel = fuel_mass;
+    let mut fuel_mass = *mass;
+    let mut total_fuel = 0;
     while fuel_mass > 6 {
         fuel_mass = fuel_mass / 3 - 2;
         total_fuel += fuel_mass;
@@ -14,24 +14,12 @@ fn main() {
     let input: Vec<i32> = fs::read_to_string("input.txt")
         .expect("Failed reading file")
         .lines()
-        .map(|line| {
-            line
-                .parse()
-                .expect("Failed parsing line into integer")
-        })
+        .map(|line| line.parse().expect("Failed parsing line into integer"))
         .collect();
 
-    let part1 = input
-        .iter()
-        .fold(0, |acc, mass| {
-            acc + (mass / 3 - 2)
-        });
+    let part1 = input.iter().fold(0, |acc, mass| acc + (mass / 3 - 2));
     println!("{}", part1);
 
-    let part2 = input
-        .iter()
-        .fold(0, |acc, mass| {
-            acc + fuel(mass)
-        });
+    let part2 = input.iter().fold(0, |acc, mass| acc + fuel(mass));
     println!("{}", part2);
 }
