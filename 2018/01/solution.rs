d17727848747f944adb1d423711cc5a438188c96
Andre Wong <andre@andre-wong.com>
2020-06-13 13:53:31 +1000

    2018 day 1 rust

---
+++ 2018/01/solution.rs
@@ -0,0 +1,33 @@
+use std::fs;
+use std::collections::HashSet;
+
+fn main() {
+    let input = parse_input();
+    println!("{}", part1(&input));
+    println!("{}", part2(&input).unwrap());
+}
+
+fn parse_input() -> Vec<i32> {
+    fs::read_to_string("input.txt")
+        .unwrap()
+        .lines()
+        .map(|line| line.parse().unwrap())
+        .collect()
+}
+
+fn part1(input: &Vec<i32>) -> i32 {
+    input.iter().fold(0, |acc, frequency| acc + frequency)
+}
+
+fn part2(input: &Vec<i32>) -> Option<i32> {
+    let mut cumulative = 0;
+    let mut seen = HashSet::new();
+    for delta in input.iter().cycle() {
+        cumulative += delta;
+        if seen.contains(&cumulative) {
+            return Some(cumulative);
+        }
+        seen.insert(cumulative);
+    }
+    None
+}
