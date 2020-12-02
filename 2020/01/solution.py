54340518b86b6a212866a3fae77107963d326301
Andre Wong <andre@andre-wong.com>
2020-12-02 23:13:32 +1000

    Improvements

--- 2020/01/solution.py
+++ 2020/01/solution.py
@@ -1,20 +1,20 @@
+from itertools import combinations
+
+
 def parse_input():
     with open('input.txt') as f:
         lines = f.readlines()
     return list(map(int, lines))
 
 def part1(inputs):
-    for i, x in enumerate(inputs):
-        for y in inputs[i + 1:]:
-            if x + y == 2020:
-                return x * y
+    for x, y in combinations(inputs, 2):
+        if x + y == 2020:
+            return x * y
 
 def part2(inputs):
-    for i, x in enumerate(inputs):
-        for j, y in enumerate(inputs[i + 1:]):
-            for z in inputs[j + 1:]:
-                if x + y + z == 2020:
-                    return x * y * z
+    for x, y, z in combinations(inputs, 3):
+        if x + y + z == 2020:
+            return x * y * z
 
 
 inputs = parse_input()
