a3d88079fe8d960d47694f0b1a77485c8652233a
Andre Wong <andre@andre-wong.com>
2020-12-02 23:11:03 +1000

    2020 Day 2

---
+++ 2020/02/solution.py
@@ -0,0 +1,36 @@
+def parse_input():
+    with open('input.txt') as f:
+        lines = f.readlines()
+    return list(map(parse_line, lines))
+
+def part1(inputs):
+    valid_count = 0
+    for ((lower_bound, upper_bound), letter), password in inputs:
+        letter_count = 0
+        for c in password:
+            if c == letter:
+                letter_count += 1
+        if lower_bound <= letter_count <= upper_bound:
+            valid_count += 1
+    return valid_count
+
+def part2(inputs):
+    valid_count = 0
+    for (positions, letter), password in inputs:
+        c1 = password[positions[0] - 1]
+        c2 = password[positions[1] - 1]
+        if ((c1 == letter or c2 == letter) and c1 != c2):
+            valid_count += 1
+    return valid_count
+
+def parse_line(line):
+    bounds, letter, password = line.split(' ')
+    bounds = tuple(map(int, bounds.split('-')))
+    letter = letter[0]
+    return (bounds, letter), password.strip()
+
+
+inputs = parse_input()
+
+print(part1(inputs))
+print(part2(inputs))
