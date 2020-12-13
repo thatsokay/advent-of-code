cea51858c1f73002a92d860dc77dde63cb13a40e
Andre Wong <andre@andre-wong.com>
2020-12-13 14:43:13 +1000

    Improvements

--- 2020/07/solution.py
+++ 2020/07/solution.py
@@ -1,42 +1,41 @@
+from collections import defaultdict
+
+
 def parse_input():
     with open('input.txt') as f:
         lines = f.readlines()
-    return parse_bags(lines)
+    return list(map(parse_bag, lines))
 
 def part1(inputs):
-    def contains_shiny_gold(colour):
-        bag = inputs[colour]
-        if 'shiny gold' in bag:
-            return True
-        for child in bag.keys():
-            if contains_shiny_gold(child):
-                return True
-        return False
+    bags = defaultdict(list)
+    for colour, children in inputs:
+        for child in children.keys():
+            bags[child].append(colour)
 
-    count = 0
-    for colour in inputs.keys():
-        if contains_shiny_gold(colour):
-            count += 1
-    return count
+    def count_ancestors(colour):
+        parents = bags[colour]
+        return set(parents).union(*map(count_ancestors, parents))
+
+    return len(count_ancestors('shiny gold'))
 
 def part2(inputs):
+    bags = {colour: children for colour, children in inputs}
+
     def count_bags(colour):
-        bag = inputs[colour]
+        bag = bags[colour]
         return sum(bag.values()) + sum(
             quantity * count_bags(child_colour)
             for child_colour, quantity
             in bag.items()
         )
+
     return count_bags('shiny gold')
 
-def parse_bags(lines):
-    return {colour: children for colour, children in map(parse_rule, lines)}
-
-def parse_rule(line):
+def parse_bag(line):
     parent_colour, rest = line.strip().split(' bags contain ')
     if rest == 'no other bags.':
         return parent_colour, {}
-    children = rest[:-1].split(', ')
+    children = rest.split(', ')
     return parent_colour, {
         colour: quantity
         for colour, quantity
@@ -44,8 +43,9 @@ def parse_rule(line):
     }
 
 def parse_child(child):
-    parts = child.split(' ')
-    return ' '.join(parts[1:-1]), int(parts[0])
+    quantity, rest = child.split(' ', 1)
+    colour, _ = rest.rsplit(' ', 1)
+    return colour, int(quantity)
 
 
 inputs = parse_input()
