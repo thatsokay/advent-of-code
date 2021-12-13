c19afe17376a2c49d004fb47fb20d84e127dcec0
Andre Wong <andre@andre-wong.com>
2021-12-14 00:00:55 +1000

    2021 day 9

--- 2021/09/solution.rs
+++ 2021/09/solution.rs
@@ -1,4 +1,7 @@
+use std::collections::{HashMap, HashSet};
 use std::fs;
+
+type Cave = HashMap<(i32, i32), u8>;
 
 fn main() {
     let input = parse_input();
@@ -6,48 +9,64 @@ fn main() {
     println!("{}", part2(&input));
 }
 
-fn parse_input() -> Vec<Vec<i32>> {
+fn parse_input() -> Cave {
     fs::read_to_string("input.txt")
         .unwrap()
         .lines()
-        .map(|line| {
+        .enumerate()
+        .flat_map(move |(i, line)| {
             line.chars()
-                .map(|c| c.to_digit(10).unwrap() as i32)
-                .collect()
+                .enumerate()
+                .map(move |(j, c)| ((i as i32, j as i32), c.to_digit(10).unwrap() as u8))
         })
         .collect()
 }
 
-fn part1(input: &[Vec<i32>]) -> i32 {
+fn part1(input: &Cave) -> u32 {
     input
         .iter()
-        .enumerate()
-        .flat_map(|(i, row)| {
-            row.iter().enumerate().filter_map(move |(j, height)| {
-                let left = row.get((j as i32 - 1) as usize).or(Some(&10));
-                let right = row.get((j as i32 + 1) as usize).or(Some(&10));
-                let up = input
-                    .get((i as i32 - 1) as usize)
-                    .and_then(|row| row.get(j))
-                    .or(Some(&10));
-                let down = input
-                    .get((i as i32 + 1) as usize)
-                    .and_then(|row| row.get(j))
-                    .or(Some(&10));
-                match (left, right, up, down) {
-                    (Some(a), Some(b), Some(c), Some(d))
-                        if (height < a && height < b && height < c && height < d) =>
-                    {
-                        Some(height)
-                    }
-                    _ => None,
+        .filter_map(|(&(i, j), height)| {
+            let left = input.get(&(i, j - 1)).or(Some(&10));
+            let right = input.get(&(i, j + 1)).or(Some(&10));
+            let up = input.get(&(i - 1, j)).or(Some(&10));
+            let down = input.get(&(i + 1, j)).or(Some(&10));
+            match (left, right, up, down) {
+                (Some(a), Some(b), Some(c), Some(d)) if height < a.min(b).min(c).min(d) => {
+                    Some(height)
                 }
-            })
+                _ => None,
+            }
         })
-        .map(|height| height + 1)
+        .map(|&height| height as u32 + 1)
         .sum()
 }
 
-fn part2(input: &[Vec<i32>]) -> i32 {
-    0
+fn part2(input: &Cave) -> i32 {
+    let mut basins: Vec<HashSet<(i32, i32)>> = vec![];
+    let mut remaining: HashSet<(i32, i32)> = input
+        .iter()
+        .filter(|(_, &height)| height < 9)
+        .map(|(coords, _)| coords.clone())
+        .collect();
+    while !remaining.is_empty() {
+        let mut basin: HashSet<(i32, i32)> = HashSet::new();
+        let start = remaining.iter().next().unwrap().clone();
+        let mut stack: Vec<(i32, i32)> = vec![start];
+        while !stack.is_empty() {
+            let (i, j) = stack.pop().unwrap();
+            basin.insert((i, j));
+            for coord in [(i, j - 1), (i, j + 1), (i - 1, j), (i + 1, j)] {
+                if remaining.contains(&coord) {
+                    remaining.remove(&coord);
+                    stack.push(coord);
+                }
+            }
+        }
+        basins.push(basin);
+    }
+    basins.sort_by(|a, b| b.len().cmp(&a.len()));
+    basins[..3]
+        .iter()
+        .map(|basin| basin.len() as i32)
+        .fold(1, |acc, size| acc * size)
 }
